303ef3065b2eecd651ee722c0a72bb4c65aac645
Samitthichai <113626752+Samitthichai@users.noreply.github.com>
2023-08-02 14:19:26 +0700

    2/8/66

--- src/main/java/th/mfu/HelloController.java
+++ src/main/java/th/mfu/HelloController.java
@@ -1,16 +1,25 @@
 package th.mfu;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
-    String hello() {
-        return "Hello World!";
+    @GetMapping("/hello") /// {name} parameter name
+    String hello() { /* @PathVariable String name = name */
+        return "Hello World";
     }
 
-   
+    @GetMapping("/hi/{name}") /// {name} parameter name
+    String hi(@PathVariable String name) { /* @PathVariable String name = name */
+        return "Hello " + name;
+    }
+
+    @GetMapping("/sum/{a}/{b}")
+    int sum(@PathVariable int a, @PathVariable int b) {
+        return a + b;
+    }
+
 }
--- src/test/java/th/mfu/HelloControllerTest.java
+++ src/test/java/th/mfu/HelloControllerTest.java
@@ -1,13 +1,13 @@
 package th.mfu;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-public class HelloControllerTest {
+@SpringBootTest /// test script spring boots
+public class HelloControllerTest { /// test unit class to check output
 
     @Autowired
     private HelloController controller;
@@ -18,6 +18,29 @@ public class HelloControllerTest {
         // Act
         String response = controller.hello();
         // Assert
-        assertEquals("Hello World!", response);
+        assertEquals("Hello World", response);
+
+    }
+
+    @Test
+    public void testHi() {
+
+        // Act
+        String response = controller.hi("CMU");
+        // Assert
+        assertEquals("Hello CMU", response);
+    }
+
+    @Test
+    public void testSum() {
+
+        // Act
+        int response = controller.sum(1, 2);
+        // Assert
+        assertEquals(3, response);
+
+        int response2 = controller.sum(4, 5);
+        // Assert
+        assertEquals(9, response2);
     }
 }
